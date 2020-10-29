fb54cfa81410181e825adad6eeaa8e3d403b51bb
Prem Pathuri <prem@Prems-MacBook-Pro.local>
2020-10-29 16:44:11 -0700

    rudimentary pipeline

--- run.py
+++ run.py
@@ -1,49 +1,12 @@
-#!/usr/bin/env python
-
-import sys
-import json
-
-sys.path.insert(0, 'src/data')
-sys.path.insert(0, 'src/model')
-
-from etl import get_data
-from model import train_model
-
-
-def main(targets):
-    '''
-    Runs the main project pipeline logic, given the targets.
-    targets must contain: 'data', 'analysis', 'model'. 
+from src.data.etl import load_config, get_data_date
+import yaml
+import os
+import subprocess
+def main():
+    # config file should be in root of the repository
+    config = load_config("./config/config.yaml")
+    get_data_date(config['date'], out_dir='./src/data')
     
-    `main` runs the targets in order of data=>analysis=>model.
-    '''
-
-    if 'data' in targets:
-        with open('config/data-params.json') as fh:
-            data_cfg = json.load(fh)
-
-        # make the data target
-        data = get_data(**data_cfg)
-
-    if 'analysis' in targets:
-        with open('config/analysis-params.json') as fh:
-            analysis_cfg = json.load(fh)
-
-        # make the data target
-        compute_aggregates(data, **analysis_cfg)
-
-    if 'model' in targets:
-        with open('config/model-params.json') as fh:
-            model_cfg = json.load(fh)
-
-        # make the data target
-        train(data, **model_cfg)
-
-    return
-
 
 if __name__ == '__main__':
-    # run via:
-    # python main.py data model
-    targets = sys.argv[1:]
-    main(targets)
+    main()
--- src/data/etl.py
+++ src/data/etl.py
@@ -1,15 +1,41 @@
-def get_data(fb_id, outpath=None):
-    '''
-    Retrieve data for fb_id (facebook id) from the internet
-    and return data (or write data to file, if `outpath` is
-    not `None`).
-    '''
-    
-    data = ...
+import yaml
+import os
+import subprocess
+import pandas as pd
 
-    if outpath is None:
-        return data
+
+def get_data_date(date, out_dir, clean=False):
+    '''
+    Retrieves data for the given day from the internet and places it in src/data.
+
+    'clean' specifies whether to download the cleaned data (without retweets) or the raw data
+    '''
+
+    dailies_path = 'https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/'
+    day_path = dailies_path + date + '/'
+    if clean:
+        file_name = date + '_clean-dataset.tsv.gz'
     else:
-        ... # write data to outpath
+        file_name = date + '-dataset.tsv.gz'
+    # https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/2020-03-27/2020-03-27-dataset.tsv.gz
+    file_url = day_path + file_name
 
-        return data
+    curl_command = "curl " + file_url + " --output " + out_dir + '/' + file_name
+    file_path = out_dir + '/' + file_name
+    subprocess.run(curl_command, shell=True)
+    subprocess.run("gunzip " + file_path, shell=True)
+    data_table = pd.read_csv(file_path[:-3], sep='\t') # [:-3] removes ".gz" from the file path since its been unzipped
+    data_table = data_table['tweet_id'] # getting only the tweet ids
+    # saving to txt
+    data_table.to_csv(file_path[:-7] + '.txt', header=None, index=None, sep=' ', mode='a') # [:-7] removes .tsv.gz
+    # so now src/data should have a tsv file and a txt file for the data on the given date
+
+    # hydration of tweet ids
+    hydrate_command = "twarc hydrate " + file_path[:-7] + '.txt' + " > " + file_path[:-7] + ".jsonl"
+    subprocess.run(hydrate_command, shell=True)
+
+def load_config(path):
+    """
+    Load the configuration from config.yaml.
+    """
+    return yaml.load(open(path, 'r'), Loader=yaml.SafeLoader)
