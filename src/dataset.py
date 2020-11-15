0ae7b092a9b3de1ca7c1ee0c43c3969b13455f2f
Chris Ly <Christopherly99@gmail.com>
2020-11-15 01:58:13 -0800

    Flatten directories for easier imports

---
+++ src/dataset.py
@@ -0,0 +1,147 @@
+import yaml
+import os
+import json
+from collections import Counter
+from datetime import datetime
+
+class Tweet_Dataset:
+    
+    def __init__(self, config_path, data_folder):
+        '''params: config file path, path to folder containing data'''
+        self.config = self.load_config(config_path)
+        self.start_date = self.config['start_date']
+        self.end_date = self.config['end_date']
+        data_file = self.start_date + '_' + self.end_date + '_ids.jsonl'
+        self.jsonl_path = data_folder + "/" + data_file
+        
+        # helper function to get the hashtags in a given tweet
+        self.get_hashtags = lambda twt: [elem['text'] for elem in twt['entities']['hashtags']]
+        
+    def get_start_date(self):
+        return datetime.strptime(self.start_date, '%Y-%m-%d')
+    
+    def get_end_date(self):
+        return datetime.strptime(self.end_date, '%Y-%m-%d')
+    
+    def load_config(self, config_path):
+        """Load the configuration from config."""
+        return yaml.load(open(config_path, 'r'), Loader=yaml.SafeLoader)
+
+    def tweets(self):
+        '''
+        an iterator for the tweets
+        
+        use this by doing the following: 
+            data_generator = tweet_dataset.tweets()
+            tweet = next(data_generator)
+        '''
+        for line in open(self.jsonl_path):
+            dic = json.loads(line)
+            dic = self.clean_tweet(dic)
+            yield dic
+    
+    def hashtag_counts(self):
+        '''returns a collections.Counter object for a dict of # counts'''
+        gen = self.tweets()
+        hashtag_counts = {}
+        while True:
+            try:
+                twt = next(gen)
+                for tag in twt['entities']['hashtags']:
+                    if tag['text'] not in hashtag_counts:
+                        hashtag_counts[tag['text']] = 1
+                    else:
+                        hashtag_counts[tag['text']] += 1
+            except StopIteration:
+                break
+        return Counter(hashtag_counts)
+    
+    def user_id_counts(self):
+        '''returns a collections.Counter object for a dict of user counts'''
+        gen = self.tweets()
+        user_counts = {}
+        while True:
+            try:
+                twt = next(gen)
+                usr_id = twt['user']['id_str']
+                if usr_id not in user_counts:
+                    user_counts[usr_id] = 1
+                else:
+                    user_counts[usr_id] += 1
+            except StopIteration:
+                break
+        return Counter(user_counts)
+    
+    def user_name_counts(self):
+        '''returns a collections.Counter object for a dict of user counts'''
+        gen = self.tweets()
+        user_counts = {}
+        while True:
+            try:
+                twt = next(gen)
+                usr_name = twt['user']['screen_name']
+                if usr_name not in user_counts:
+                    user_counts[usr_name] = 1
+                else:
+                    user_counts[usr_name] += 1
+            except StopIteration:
+                break
+        return Counter(user_counts)
+    
+    def get_day_tag_count(self, hashtag, date):
+        '''returns the count of a hashtag on a given day
+        params: hashtag, date, and Tweet_Dataset object
+        date format: 10-31-2020, or datetime object
+        '''
+        if isinstance(date, str):
+            date = datetime.strptime('03-22-2020', '%m-%d-%Y')
+        
+        gen = self.tweets()
+        count = 0
+        while True:
+            try:
+                twt = next(gen)
+                hashtags = self.get_hashtags(twt) # using lambda function made in __init__
+                if hashtag in hashtags:
+                    if twt['created_at'].date() == date.date():
+                        count += 1
+            except StopIteration:
+                break
+        return count
+    
+    def get_daily_tag_counts(self):
+        '''returns the counts of each hashtag by date'''
+        gen = self.tweets()
+        daily_tag_occurrences = {} # {tag: {date: value}}
+        while True:
+            try:
+                twt = next(gen)
+                twt_date = twt['created_at'].date()
+                hashtags = self.get_hashtags(twt)
+                for tag in hashtags:
+                    if tag in daily_tag_occurrences:
+                        if twt_date in daily_tag_occurrences[tag]:
+                            daily_tag_occurrences[tag][twt_date] += 1
+                        else:
+                            daily_tag_occurrences[tag][twt_date] = 1
+                    else:
+                        daily_tag_occurrences[tag] = {twt_date: 1}
+            except StopIteration:
+                break
+        return daily_tag_occurrences
+    
+    def __len__(self):
+        num_tweets = 0
+        gen = self.tweets()
+        while True:
+            try:
+                twt = next(gen)
+                num_tweets += 1
+            except StopIteration:
+                break
+        return num_tweets
+    
+    def clean_tweet(self, twt_dic):
+        '''pre processing on the tweets'''
+        twt_dic['created_at'] = datetime.strptime(twt_dic['created_at'], '%a %b %d %X %z %Y')
+        return twt_dic
---
+++ src/etl.py
@@ -0,0 +1,231 @@
+import os
+import subprocess
+import numpy as np
+
+import yaml
+import json
+from datetime import timedelta, date
+from utils import load_config
+
+
+# TODO: add twarc authentification
+
+def get_data(start_date, end_date, outdir, clean=False, p=360):
+    """Downloads tweets for the days between start and end dates.
+    
+    Dates range from start_date inclusive to end_date exclusive. Attempts
+    downloading data if data file doesn't already exist.
+       
+    Args:
+        start_date (str): start of data range. Format YYYY-MM-DD.
+        end_date (str): end of data range. Format YYYY-MM-DD.
+        outdir (str): data directory path.
+        clean (bool): specifies whether to download the cleaned data 
+          (without retweets) or the raw data.
+        
+    """
+    parent_dir = os.path.dirname(outdir)
+    file_txt_path = os.path.join(parent_dir, 'temp', start_date) + f'_{end_date}_ids.txt'  # ./data/temp/2020-03-27_2020-03-30_ids.txt
+    file_jsonl_path = os.path.splitext(file_txt_path)[0] + '.jsonl'
+    
+    if os.path.isfile(file_jsonl_path):
+        pass
+    else:
+        get_data_range_p(start_date, end_date, outdir, clean)
+        hydrate_data_range(start_date, end_date, outdir, clean)
+    return
+
+def get_data_range_p(start_date, end_date, outdir, clean=False, p=360):
+    """Downloads tweets for the days between start and end dates.
+    
+    Dates range from start_date inclusive to end_date exclusive. Attempts
+    downloading data if data file doesn't already exist.
+       
+    Args:
+        start_date (str): start of data range. Format YYYY-MM-DD.
+        end_date (str): end of data range. Format YYYY-MM-DD.
+        outdir (str): data directory path.
+        clean (bool): specifies whether to download the cleaned data 
+          (without retweets) or the raw data.
+        
+    """
+    parent_dir = os.path.dirname(outdir)
+    file_txt_path = os.path.join(parent_dir, 'temp', start_date) + f'_{end_date}_ids.txt'  # ./data/temp/2020-03-27_2020-03-30_ids.txt
+    
+    if os.path.isfile(file_txt_path):
+        return
+    else:
+        start = date.fromisoformat(start_date)
+        end = date.fromisoformat(end_date)
+        out = []
+        for day in daterange(start, end):
+            date_ = day.isoformat()
+            try:
+                download_data_date(date_, outdir, clean)
+                tmp = read_data_date(date_, outdir, clean)
+                out = out + list(tmp[::p].copy())  # running list of every p tweet IDs
+            except:
+                pass
+        out = np.array(out)
+
+        np.savetxt(file_txt_path, out, fmt='%i')
+    return
+
+def download_data_date(date, outdir, clean=False):
+    """Downloads tweets for the given day.
+    
+    First checks if tweets have already been downloaded. If they haven't,
+    then download them and save the tweet IDs to a txt file.
+       
+    Args:
+        date (str): date of data. Format YYYY-MM-DD.
+        outdir (str): data directory path.
+        clean (bool): specifies whether to download the cleaned data 
+          (without retweets) or the raw data.
+        
+    """
+    data_path = os.path.join(outdir, date) + '_all_ids.txt' # ./data/raw/2020-03-27_all_ids.txt
+    if os.path.isfile(data_path):
+        return
+    else:  # full data doesn't exist
+        if clean:
+            file_name = date + '_clean-dataset.tsv.gz'
+        else:
+            file_name = date + '-dataset.tsv.gz'
+
+        url_base = 'https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/' + date
+        # https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/2020-03-27/2020-03-27-dataset.tsv.gz
+        url = os.path.join(url_base, file_name)
+        gz_path = os.path.join(outdir, file_name) # ./data/raw/2020-03-27-dataset.tsv.gz
+        
+        curl_command = "curl " + url + " --output " + gz_path
+        subprocess.run(curl_command, shell=True)
+        try:
+            # np.genfromtxt decompresses .gz by default
+            twitter_ids = np.genfromtxt(gz_path, dtype=np.int64, skip_header=1, usecols=(0,))
+            np.savetxt(data_path, twitter_ids, fmt='%i')
+        except:
+            pass
+        os.remove(gz_path)
+    return
+
+def read_data_date(date, outdir, clean=False):
+    return np.genfromtxt(f'data/raw/{date}_all_ids.txt', dtype=np.int64)
+
+def hydrate_data_range(start_date, end_date, outdir, clean=False):
+    """Sample 1 tweet for every p tweets on the given day.
+    
+    Hydrates tweets in date range sampling 1 tweet for every p tweets.
+    This function should only be run after get_data_range() is called. It
+    will save the sampled tweet IDs in another file.
+       
+    Args:
+        start_date (str): start of data range. Format YYYY-MM-DD.
+        end_date (str): end of data range. Format YYYY-MM-DD.
+        outdir (str): data directory path.
+        clean (bool): specifies whether to download the cleaned data 
+          (without retweets) or the raw data.
+        p (int): rate of tweets to hydrate.
+        
+    """
+    parent_dir = os.path.dirname(outdir)
+    file_txt_path = os.path.join(parent_dir, 'temp', start_date) + f'_{end_date}_ids.txt'  # ./data/temp/2020-03-27_2020-03-30_ids.txt
+    file_jsonl_path = os.path.splitext(file_txt_path)[0] + '.jsonl'                        # replace ".txt" from the file path with ".jsonl"
+    
+    hydrate_ids(file_txt_path, file_jsonl_path)
+
+def get_N_data_date(date, outdir, clean=False, N=500):
+    """Retrieves N tweets for the given day.
+    
+    Retrieves N tweets from existing file. If it doesn't exist then sample
+    N tweets from file containing all tweet IDs for the given day. And if
+    that file also doesn't exist, fetch it from the github and create them.
+       
+    Args:
+        date (str): date of data. Format YYYY-MM-DD.
+        outdir (str): data directory path.
+        clean (bool): specifies whether to download the cleaned data 
+          (without retweets) or the raw data.
+        N (int): number of tweets to hydrate, if None hydrate all.
+        
+    Returns:
+        A list containing N tweet json objects.
+        
+    """
+    file_txt_path = os.path.join(outdir, date) + f'_{N}_ids.txt'     # ./data/raw/2020-03-27_500_ids.txt
+    file_jsonl_path = os.path.splitext(file_txt_path)[0] + '.jsonl'  # replace ".txt" from the file path with ".jsonl"
+    
+    try:
+        return read_jsonl(file_jsonl_path)
+    except FileNotFoundError:  # jsonl file doesn't exist for N sample
+        pass
+    try:
+        # read in full data to sample from
+        full_data_path = os.path.join(outdir, date) + '_all_ids.txt' # ./data/raw/2020-03-27_all_ids.txt
+        twitter_ids = np.genfromtxt(full_data_path, dtype=np.int64)  
+        
+        sample_idx = np.random.uniform(high=len(twitter_ids), size=N).astype(np.int64)
+        sample = twitter_ids[sample_idx]
+        np.savetxt(file_txt_path, sample, fmt='%i')
+        
+        hydrate_ids(file_txt_path, file_jsonl_path)
+        return read_jsonl(file_jsonl_path)
+    except:  # full data doesn't exist
+        if clean:
+            file_name = date + '_clean-dataset.tsv.gz'
+        else:
+            file_name = date + '-dataset.tsv.gz'
+
+        url_base = 'https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/' + date
+        # https://raw.githubusercontent.com/thepanacealab/covid19_twitter/master/dailies/2020-03-27/2020-03-27-dataset.tsv.gz
+        url = os.path.join(url_base, file_name)
+        gz_path = os.path.join(outdir, file_name) # ./data/raw/2020-03-27-dataset.tsv.gz
+        
+        curl_command = "curl " + url + " --output " + gz_path
+        subprocess.run(curl_command, shell=True)
+        
+        # np.genfromtxt decompresses .gz by default
+        twitter_ids = np.genfromtxt(gz_path, dtype=np.int64, skip_header=1, usecols=(0,))
+        np.savetxt(full_data_path, twitter_ids, fmt='%i')
+        
+        os.remove(gz_path)
+
+        sample_idx = np.random.uniform(high=len(twitter_ids), size=N).astype(np.int64)
+        sample = twitter_ids[sample_idx]
+        np.savetxt(file_txt_path, sample, fmt='%i')
+        
+        hydrate_ids(file_txt_path, file_jsonl_path)
+        return read_jsonl(file_jsonl_path)
+
+def read_jsonl(jsonl_path):
+    """Returns a list of tweet json objects."""
+    data = []
+    with open(jsonl_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            data.append(json.loads(line))
+    return data
+                
+def hydrate_ids(txt_path, jsonl_path):
+    """Retrieves tweets given IDs in txt_path using twarc"""
+    hydrate_command = "twarc hydrate " + txt_path + " > " + jsonl_path
+    subprocess.run(hydrate_command, shell=True)
+
+# def load_config(path):
+#     """Load the configuration from config."""
+#     return yaml.load(open(path, 'r'), Loader=yaml.SafeLoader)
+
+def daterange(start, end):
+    """Generator for dates between start and end dates.
+    
+    ref: https://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
+    
+    Args:
+        start (date): start of range inclusive.
+        end (date): end of range exclusive.
+    
+    Yields:
+        Next date in range.
+        
+    """
+    for n in range(int((end - start).days)):
+        yield start + timedelta(n)
