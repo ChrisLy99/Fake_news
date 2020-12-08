2d22be3c4d9a82a2376ae86337e21cbf07026873
Chris Ly <Christopherly99@gmail.com>
2020-12-07 17:58:03 -0800

    Remove old implementation

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,3 @@
-# import yaml
 import os
 import json
 from collections import Counter
@@ -8,25 +7,10 @@ class Tweet_Dataset:
     
     def __init__(self, filepath):
         '''params: config file path, path to folder containing data'''
-#         self.config = self.load_config(config_path)
-#         self.start_date = self.config['start_date']
-#         self.end_date = self.config['end_date']
-#         data_file = self.start_date + '_' + self.end_date + '_ids.jsonl'
-#         self.jsonl_path = data_folder + "/" + data_file
         self.data = filepath
         
         # helper function to get the hashtags in a given tweet
         self.get_twt_hashtags = lambda twt: [tag['text'] for tag in twt['entities']['hashtags']]
-        
-#     def get_start_date(self):
-#         return datetime.strptime(self.start_date, '%Y-%m-%d')
-    
-#     def get_end_date(self):
-#         return datetime.strptime(self.end_date, '%Y-%m-%d')
-    
-#     def load_config(self, config_path):
-#         """Load the configuration from config."""
-#         return yaml.load(open(config_path, 'r'), Loader=yaml.SafeLoader)
 
     def tweets(self):
         '''
@@ -43,19 +27,6 @@ class Tweet_Dataset:
     
     def hashtag_counts(self):
         '''returns a collections.Counter object for a dict of # counts'''
-#         gen = self.tweets()
-#         hashtag_counts = {}
-#         while True:
-#             try:
-#                 twt = next(gen)
-#                 for tag in twt['entities']['hashtags']:
-#                     if tag['text'] not in hashtag_counts:
-#                         hashtag_counts[tag['text']] = 1
-#                     else:
-#                         hashtag_counts[tag['text']] += 1
-#             except StopIteration:
-#                 break
-#         return Counter(hashtag_counts)
         cnt = Counter()
         for twt in self.tweets():
             tags = self.get_twt_hashtags(twt)
@@ -64,19 +35,6 @@ class Tweet_Dataset:
     
     def user_id_counts(self):
         '''returns a collections.Counter object for a dict of user counts'''
-#         gen = self.tweets()
-#         user_counts = {}
-#         while True:
-#             try:
-#                 twt = next(gen)
-#                 usr_id = twt['user']['id_str']
-#                 if usr_id not in user_counts:
-#                     user_counts[usr_id] = 1
-#                 else:
-#                     user_counts[usr_id] += 1
-#             except StopIteration:
-#                 break
-#         return Counter(user_counts)
         cnt = Counter()
         for twt in self.tweets():
             uid = twt['user']['id_str']
@@ -85,19 +43,6 @@ class Tweet_Dataset:
     
     def user_name_counts(self):
         '''returns a collections.Counter object for a dict of user counts'''
-#         gen = self.tweets()
-#         user_counts = {}
-#         while True:
-#             try:
-#                 twt = next(gen)
-#                 usr_name = twt['user']['screen_name']
-#                 if usr_name not in user_counts:
-#                     user_counts[usr_name] = 1
-#                 else:
-#                     user_counts[usr_name] += 1
-#             except StopIteration:
-#                 break
-#         return Counter(user_counts)
         cnt = Counter()
         for twt in self.tweets():
             u_name = twt['user']['screen_name']
@@ -112,18 +57,6 @@ class Tweet_Dataset:
         if isinstance(day, str):
             day = datetime.strptime('03-22-2020', '%m-%d-%Y')
         
-#         gen = self.tweets()
-#         count = 0
-#         while True:
-#             try:
-#                 twt = next(gen)
-#                 hashtags = self.get_hashtags(twt) # using lambda function made in __init__
-#                 if hashtag in hashtags:
-#                     if twt['created_at'].date() == date.date():
-#                         count += 1
-#             except StopIteration:
-#                 break
-#         return count
         cnt = 0
         for twt in self.tweets():
             if twt['created_at'].date() == day.date():
@@ -131,28 +64,9 @@ class Tweet_Dataset:
                 if hashtag in tags:
                     cnt += 1
         return cnt
-#     lambda twt: [elem['text'] for elem in twt['entities']['hashtags']]
     
     def get_daily_tag_counts(self):
         '''returns the counts of each hashtag by date'''
-#         gen = self.tweets()
-#         daily_tag_occurrences = {} # {tag: {date: value}}
-#         while True:
-#             try:
-#                 twt = next(gen)
-#                 twt_date = twt['created_at'].date()
-#                 hashtags = self.get_hashtags(twt)
-#                 for tag in hashtags:
-#                     if tag in daily_tag_occurrences:
-#                         if twt_date in daily_tag_occurrences[tag]:
-#                             daily_tag_occurrences[tag][twt_date] += 1
-#                         else:
-#                             daily_tag_occurrences[tag][twt_date] = 1
-#                     else:
-#                         daily_tag_occurrences[tag] = {twt_date: 1}
-#             except StopIteration:
-#                 break
-#         return daily_tag_occurrences
         cnt = {}  # {tag: {date: value}}
         for twt in self.tweets():
             twt_date = twt['created_at'].date()
@@ -164,15 +78,6 @@ class Tweet_Dataset:
         return cnt
     
     def __len__(self):
-#         num_tweets = 0
-#         gen = self.tweets()
-#         while True:
-#             try:
-#                 twt = next(gen)
-#                 num_tweets += 1
-#             except StopIteration:
-#                 break
-#         return num_tweets
         with open(self.data) as f:
             num_twts = sum(1 for _ in f)
         return num_twts
@@ -180,4 +85,6 @@ class Tweet_Dataset:
     def clean_tweet(self, twt_dic):
         '''pre processing on the tweets'''
         twt_dic['created_at'] = datetime.strptime(twt_dic['created_at'], '%a %b %d %X %z %Y')
+        for ht_ent in twt_dic['entities']['hashtags']:
+            ht_ent.update({'text': str.lower(ht_ent['text'])})
         return twt_dic
